730812586a6f0853e55f09f351ab23090d1db9c3
Agu C Christopher <aguchris740@gmail.com>
2022-11-19 11:15:38 -0800

    Fourth Commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://agucchristopher:aguchristopher1231123@memories-app.ryucxb9.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://agucchristopher:agucchristopher1231123@memories-app.ryucxb9.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
