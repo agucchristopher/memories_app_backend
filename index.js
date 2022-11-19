f8b57eed37a17a6cd76f60bf45becf90b5a095d8
Agu C Christopher <aguchris740@gmail.com>
2022-11-19 12:20:02 -0800

    Eight Commit

--- index.js
+++ index.js
@@ -21,5 +21,4 @@ const PORT = process.env.PORT|| 5000;
 const client = mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-  mongoose.Schema(PostMessage)
 mongoose.set('useFindAndModify', false);
